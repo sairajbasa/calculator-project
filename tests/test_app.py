8e70894f35ffa0d0c36965a1f9738ff467dbd551
sairajbasa <97380646+sairajbasa@users.noreply.github.com>
2026-08-13 00:41:12 +0530

    Add unit tests for arithmetic functions

---
+++ tests/test_app.py
@@ -0,0 +1,17 @@
+from src.utils import add, subtract, multiply, divide
+
+
+def test_add():
+    assert add(10, 5) == 15
+
+
+def test_subtract():
+    assert subtract(10, 5) == 5
+
+
+def test_multiply():
+    assert multiply(10, 5) == 50
+
+
+def test_divide():
+    assert divide(10, 5) == 2
