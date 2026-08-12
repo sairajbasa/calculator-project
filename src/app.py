5f38ccbe7ecd5852dff84f4427334f9b3f6361a8
sairajbasa <97380646+sairajbasa@users.noreply.github.com>
2026-08-13 00:39:45 +0530

    Implement basic calculator functions in app.py

---
+++ src/app.py
@@ -0,0 +1,12 @@
+from utils import add, subtract, multiply, divide
+
+
+def calculate():
+    print("Addition:", add(10, 5))
+    print("Subtraction:", subtract(10, 5))
+    print("Multiplication:", multiply(10, 5))
+    print("Division:", divide(10, 5))
+
+
+if __name__ == "__main__":
+    calculate()
